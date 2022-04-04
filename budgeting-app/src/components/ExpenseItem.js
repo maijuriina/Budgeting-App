779e5ba9d3e60f27ec3691757b2596a0eb32d339
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-04 18:20:54 +0300

    Added Expense Item component now used in App.js

---
+++ budgeting-app/src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h3>Date</h3>
+      </div>
+      <div>
+        <h3>Expense Title</h3>
+      </div>
+      <div>
+        <h3>Amount</h3>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
