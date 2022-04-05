ed68d5aa2539d5b1522140bff1a9c348c56d2b9b
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-05 16:07:02 +0300

    useState hook and clickevent changes title

--- budgeting-app/src/components/Expenses/ExpenseItem.js
+++ budgeting-app/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); // useState always returns array of value and function setValue
+
+  const clickHandler = () => {
+    setTitle("Updated title"); // setTitle sets value and calls component functions again, updating the value in instance of component
+    // now every time the click handler is called, the expense item instance is evaluated by React
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">
         <h3>{props.amount}</h3>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
