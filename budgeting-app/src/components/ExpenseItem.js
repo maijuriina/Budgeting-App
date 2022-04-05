ee780b6ad7d0458d33bc19d237fd79dfe1b0b7c0
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-05 08:32:55 +0300

    Added css styling
     Css base copied from course

--- budgeting-app/src/components/ExpenseItem.js
+++ budgeting-app/src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>
-        <h3>Date</h3>
+    <div className="expense-item">
+      <h3>Date</h3>
+      <div className="expense-item__description">
+        <h2>Expense Title</h2>
       </div>
-      <div>
-        <h3>Expense Title</h3>
-      </div>
-      <div>
+      <div className="expense-item__price">
         <h3>Amount</h3>
       </div>
     </div>
