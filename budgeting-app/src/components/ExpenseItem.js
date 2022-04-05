fbbaf67a084fa63f282d6ecc15ef39a259ca1308
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-05 08:57:10 +0300

    Fixed error relating to root. Object references used in jsx.

--- budgeting-app/src/components/ExpenseItem.js
+++ budgeting-app/src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 3, 5).toDateString();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 355;
+
   return (
     <div className="expense-item">
-      <h3>Date</h3>
+      <h3>{expenseDate}</h3>
       <div className="expense-item__description">
-        <h2>Expense Title</h2>
+        <h2>{expenseTitle}</h2>
       </div>
       <div className="expense-item__price">
-        <h3>Amount</h3>
+        <h3>{expenseAmount}</h3>
       </div>
     </div>
   );
