371ff57b2af5bf362eb181467fcd25bea5692721
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-05 15:01:48 +0300

    Functions to arrow functions w/ const. White borders added

--- budgeting-app/src/components/UI/Card.js
+++ budgeting-app/src/components/UI/Card.js
@@ -1,9 +1,9 @@
 import "./Card.css";
 
 // by passing props and referencing props.children, it is possible to use the Card as a wrapper component
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className; // enables use of classname on Card, using {classes} on returning div
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
