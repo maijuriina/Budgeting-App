f1d5e51c8c209fa703c8cb5acb32cbc62b756ad0
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-06 16:54:32 +0300

    Open and close form when adding/canceling add

--- budgeting-app/src/components/NewExpense/NewExpense.js
+++ budgeting-app/src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isAdding, setIsAdding] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData, // this is userInput from ExpenseForm, spread to copy
@@ -10,12 +13,30 @@ const NewExpense = (props) => {
 
     // use prop to call onAddExpense in App.js, also passing expenseData originally from ExpenseForm
     props.onAddExpense(expenseData);
+
+    setIsAdding(false);
+  };
+
+  const startAddingHandler = () => {
+    setIsAdding(true); // opens form
+  };
+
+  const stopAddingHandler = () => {
+    setIsAdding(false); // closes form
   };
 
   // onSaveExpenseData is a prop which points to handler - therefore it can be accessed in ExpenseForm
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isAdding && (
+        <button onClick={startAddingHandler}>Add New Expense</button>
+      )}
+      {isAdding && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopAddingHandler}
+        />
+      )}
     </div>
   );
 };
