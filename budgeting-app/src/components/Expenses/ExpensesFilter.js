a26d7044e32cf6dccb0da0facec04c230de15def
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-06 13:23:35 +0300

    Filter by year begun, data passed up and logged

--- budgeting-app/src/components/Expenses/ExpensesFilter.js
+++ budgeting-app/src/components/Expenses/ExpensesFilter.js
@@ -1,19 +1,26 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  // used to render children
+  const classes = "expenses-filter " + props.className;
+
+  const filterHandler = (event) => {
+    props.onChangeFilter(event.target.value); // pass value to be used in props at Expenses.js
+  };
+
   return (
-    <div className="expenses-filter">
+    <div className={classes}>
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={props.selected} onChange={filterHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
           <option value="2019">2019</option>
         </select>
       </div>
+      {props.children}
     </div>
   );
 };
