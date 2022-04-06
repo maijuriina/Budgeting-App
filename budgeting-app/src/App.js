f4056f51d2b1c5ea1f7b317a95f90afea2a4eccb
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-06 15:27:13 +0300

    Changed expenses list to dynamic, new can be added
    Shows up on UI. Dummy data moved to separate const.

--- budgeting-app/src/App.js
+++ budgeting-app/src/App.js
@@ -1,34 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Hedelmiä",
+    amount: 94.35,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Autovakuutus",
+    amount: 294.65,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Pöytä (puinen)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Hedelmiä",
-      amount: 94.35,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Autovakuutus",
-      amount: 294.65,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Pöytä (puinen)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   // used as a prop on NewExpense to make a call through pointing to addExpenseHandler
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      // pass function with prevExpenses to add previous state snapshot with new expense
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
